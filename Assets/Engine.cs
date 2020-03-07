4b083577033e2051a8ba4a2b27f8b87f9351b976
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2020-03-07 04:52:53 +0000

    put in default values and added fuel weight

--- Assets/Engine.cs
+++ Assets/Engine.cs
@@ -4,38 +4,73 @@ using UnityEngine;
 
 public class Engine : MonoBehaviour
 {
-    [SerializeField] private Transform height; // how much force is added to currentEngineForce per second
-    [SerializeField] private float engineRevUp; // how much force is added to currentEngineForce per second
-    [SerializeField] private float maxEngineForce;
-    private float currentEngineForce;
-    private Rigidbody rigidBody;
-    private Vector3 up;
+    float shipWeight;
+    // genes
+    [SerializeField] private float peakThrust;              // force in mega-newtons the engine can apply
+    [SerializeField] private float burnStartHeight;         // the height the burn should start
+    [SerializeField] private float startingFuel;            // the starting fuel in kilonewtons
+
+    // derrived variables
+    [SerializeField] private Rigidbody rigidBody;           // rigid body of the ship
+    [SerializeField] private Transform heightTransform;     // bottom of ship, used to get distance from ground
+    [SerializeField] private float engineRevUpTime;         // seconds to reach peakThrust
+
+    // 
+    private float currentThrust;
+
+    // logic variables
     private float currentVelocity;
+    private bool stopBurn;
+    private float lastHeight;
+    private float fuelBurned;
+    private Vector3 up;
+    private float GetFuelWeight(float newtons)
+    {
+        return newtons / 9.806f;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        currentEngineForce = 0f;
-        rigidBody = gameObject.GetComponent<Rigidbody>();
+        shipWeight = rigidBody.mass;
+        fuelBurned = 0f;
+        lastHeight = heightTransform.position.y + 1f;
+        currentThrust = 0f;
+        stopBurn = false;
         up = transform.up;
     }
     void FixedUpdate()
     {
         currentVelocity = rigidBody.velocity.magnitude;
+        rigidBody.mass = Mathf.Min(GetFuelWeight(shipWeight + startingFuel - fuelBurned), shipWeight);
     }
     // Update is called once per frame
     void Update()
     {
-        float deltaTime = Time.deltaTime;
-        if (currentEngineForce < maxEngineForce)
+        if (!stopBurn && (lastHeight < heightTransform.position.y || fuelBurned > startingFuel))
         {
-            currentEngineForce += deltaTime * engineRevUp;
+            stopBurn = true;
+            Debug.Log("Ship stopped at height: " + heightTransform.position.y.ToString());
         }
-        currentEngineForce = Mathf.Min(currentEngineForce, maxEngineForce);
-        rigidBody.AddForce(up * currentEngineForce * deltaTime);
+        float deltaTime = Time.deltaTime;
+        if (heightTransform.position.y <= burnStartHeight && !stopBurn)
+        {
+            if (currentThrust < peakThrust)
+            {
+                currentThrust += deltaTime * (peakThrust / engineRevUpTime);
+            }
+            currentThrust = Mathf.Min(currentThrust, peakThrust);
+            rigidBody.AddForce(up * currentThrust);
+            fuelBurned += currentThrust * deltaTime;
+        }
+        lastHeight = heightTransform.position.y;
     }
     void OnCollisionEnter(Collision x)
     {
-        Debug.Log(currentVelocity);
-        maxEngineForce = 0f;
+        if (currentVelocity > 0f)
+        {
+            Debug.Log("Hit ground at: " + currentVelocity.ToString() + " m/s");
+            Debug.Log("Total expended fuel was: " + fuelBurned.ToString());
+        }
+        stopBurn = true;
     }
 }
